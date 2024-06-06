64e7402b3fc93f10efe9b85040cdb893eac8e5ba
rinapachkale <108327466+rinapachkale@users.noreply.github.com>
2024-06-07 00:33:30 +0530

    Update EmsystemApplication.java

--- src/main/java/com/springbootworld/emsystem/EmsystemApplication.java
+++ src/main/java/com/springbootworld/emsystem/EmsystemApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmsystemApplication {
+public class EmsystemApplication { //Entry point
 
 	public static void main(String[] args) {
 
